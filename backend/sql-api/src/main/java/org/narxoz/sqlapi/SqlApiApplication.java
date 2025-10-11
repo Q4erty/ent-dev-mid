85a6dbd5535721b0addf7979df5767a5adef11a5
Q4erty <nurtas.baizhanov.dev@gmail.com>
2025-10-11 17:45:03 +0500

    init sql api

---
+++ backend/sql-api/src/main/java/org/narxoz/sqlapi/SqlApiApplication.java
@@ -0,0 +1,13 @@
+package org.narxoz.sqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlApiApplication.class, args);
+    }
+
+}
